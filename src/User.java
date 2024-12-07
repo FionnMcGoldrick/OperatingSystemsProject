dd9388f0a2a2cad0a45ba341b94f541cb807a0fe
Fionn McGoldrick <G00422349@atu.ie>
2024-12-07 23:56:09 +0000

    User made, constructor made with setters and getters.

--- src/User.java
+++ src/User.java
@@ -1,2 +1,54 @@
 public class User {
+
+    //declare variables
+    private String firstName;
+    private String secondName;
+    private String email;
+    private String password;
+
+    //constructor to initialize the variables
+    public User(String firstName, String secondName, String email, String password) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.email = email;
+        this.password = password;
+    }
+
+
+
+
+
+    //getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(){
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
