7e2b4923cccee25512fbccde858e3943048bd826
Fionn McGoldrick <G00422349@atu.ie>
2024-12-29 14:28:48 +0000

    Methods made within the ReportManager class for displaying user specific Reports and all reports

--- src/ReportManager.java
+++ src/ReportManager.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class ReportManager {
@@ -12,7 +13,7 @@ public class ReportManager {
 
     //method to save report to txt file
     public void saveReport(Report report) {
-        try{
+        try {
 
             //writing the report to the file
             bufferedWriter = new BufferedWriter(new FileWriter(reportFileName, true));
@@ -20,7 +21,41 @@ public class ReportManager {
             bufferedWriter.close();
             System.out.println("Report saved successfully\n\n< DISPLAYING ALL REPORTS >");
             //displayReports();
-        }catch (IOException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //method to view specific reports based on the user email
+    public void viewUserReports(String email) {
+        try {
+            bufferedReader = new BufferedReader(new FileReader(reportFileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] reportDetails = line.split(" ");
+                if (reportDetails[3].equals(email)) {
+                    System.out.println("Report Type: " + reportDetails[0] + "\nReport ID: " + reportDetails[1] + "\nReport Date: " + reportDetails[2] + "\nCreated By: " + reportDetails[3] + "\nStatus: " + reportDetails[4] + "\n");
+                }
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    //method to display all reports
+    public void displayReports() {
+        try {
+            bufferedReader = new BufferedReader(new FileReader(reportFileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] reportDetails = line.split(" ");
+                System.out.println("Report Type: " + reportDetails[0] + "\nReport ID: " + reportDetails[1] + "\nReport Date: " + reportDetails[2] + "\nCreated By: " + reportDetails[3] + "\nStatus: " + reportDetails[4] + "\n");
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
